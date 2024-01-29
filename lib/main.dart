99d97cadacbf47b8481150baaf25105553783416
mahdiramazani <mahdiramazani1281@gmail.com>
2024-01-29 23:04:49 +0330

    add button to Welcom page

--- lib/main.dart
+++ lib/main.dart
@@ -29,19 +29,39 @@ class WelcomPage extends StatelessWidget {
                 style: TextStyle(fontSize: 33, fontWeight: FontWeight.w900),
               ),
               Image(
-                image: AssetImage("assets/w.png"),
+                image: AssetImage("assets/welcome.png"),
               ),
               OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(
+                      color: const Color.fromARGB(255, 246, 246, 246),
+                      width: 1.3),
+                  minimumSize: Size(200, 50),
+                  shape: BeveledRectangleBorder(),
+                ),
                 onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (BuildContext context){
-                      return HomeScrean();
-                    })
-                  );
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (BuildContext context) {
+                    return HomeScrean();
+                  }));
                 },
                 child: Text(
                   "وارد شوید",
-                  style: TextStyle(color: Colors.black, fontSize: 20),
+                  style: TextStyle(
+                      color: const Color.fromARGB(255, 231, 230, 230),
+                      fontSize: 20),
+                ),
+              ),
+              SizedBox(height: 10),
+              TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: BeveledRectangleBorder(),
+                    minimumSize: Size(200, 50)),
+                onPressed: () {},
+                child: Text(
+                  "ثبت نام",
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
                 ),
               )
             ],
