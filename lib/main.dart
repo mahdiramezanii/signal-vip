0584876917b39d11e89c0c4be3dd271120bd8192
mahdiramazani <mahdiramazani1281@gmail.com>
2024-01-29 21:23:23 +0330

    fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(WelcomScrean());
+  runApp(Application());
 }
 
-class WelcomScrean extends StatelessWidget {
+class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text("transport")),
+      home: WelcomPage(),
+    );
+  }
+}
+
+class WelcomPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Open route'),
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => HomeScrean(),
+            ));
+            // Navigate to second route when tapped.
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScrean extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
       ),
     );
   }
