f7bf2a4e419aeefb7499e37fd2229f5464b9308b
mahdiramazani <mahdiramazani1281@gmail.com>
2024-01-29 21:28:12 +0330

    add fonts to projects

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: "vazir"),
       debugShowCheckedModeBanner: false,
       home: WelcomPage(),
     );
